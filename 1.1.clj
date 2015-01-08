fb446e2b9975770913d751ce055b1e8fb6a44d67
Emil Ng <github@alphabeticaldisorder.com>
2015-01-08 08:48:26 -0500

    update exercise 1.1

--- 1.1.clj
+++ 1.1.clj
@@ -1,19 +1,54 @@
+(comment
 
+  Below is a sequence of expressions. What is the result
+  printed by the interpreter in response to each expression?
+  Assume that the sequence is to be evaluated in the
+  order in which it is presented.
+
+)
 
 10
+;10
+
 (+ 5 3 4)
+;12
+
 (- 9 1)
+;8
+
 (/ 6 2)
+;3
+
 (+ (* 2 4) (- 4 6))
-(define a 3)
-(define b (+ a 1))
+;6
+
+(def a 3)
+;#'sicp.core/a
+
+(def b (+ a 1))
+;#'sicp.core/b
+
 (+ a b (* a b))
+;19
+
 (= a b)
+;false
+
 (if (and (> b a) (< b (* a b)))
-b a)
-(cond ((= a 4) 6)
-((= b 4) (+ 6 7 a))
-(else 25))
+  b
+  a)
+;4
+(cond (= a 4) 6
+      (= b 4) (+ 6 7 a)
+      :else 25)
+;16
+
 (+ 2 (if (> b a) b a))
-(* (cond ((> a b) a) ((< a b) b)
-(else -1)) (+ a 1))
+(* (cond (> a b) a
+         (< a b) b
+          :else -1)) (+ a 1)
+;4
+
+(cond (= 1 1) (= 2 2)
+      :else 1)
+;true
